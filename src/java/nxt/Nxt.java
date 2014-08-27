cd4e35c40549ceb5c2d385d908f759e38327c68f
BurstProject <burstcoin@hushmail.com>
2014-08-27 00:28:17 -0400

    update version number

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     private static final Properties defaultProperties = new Properties();
     static {
