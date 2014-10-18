a278d3a235657780d5353d23bef1df1b5e048311
BurstProject <burstcoin@hushmail.com>
2014-10-18 05:35:39 -0400

    applied nxt 1.2.9 critical fix

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "1.1.3";
+    public static final String VERSION = "1.1.4";
     public static final String APPLICATION = "NRS";
 
     private static final Properties defaultProperties = new Properties();
