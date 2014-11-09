f19182d5fa74f448b11eabcb6e2bdfae5f090d81
Dennis Fantoni <dennis@fantoni.dk>
2014-11-09 20:10:28 +0100

    compile.bat that compiles under windows with no dependencies on 3rdparty shell software

---
+++ compile.bat
@@ -0,0 +1,16 @@
+set CP=conf\;classes\;lib\*
+set SP=src\java\
+
+md classes
+
+"C:\Program Files\Java\jdk1.7.0_67\bin\javac.exe" -sourcepath %SP% -classpath %CP% -d classes\ src\java\nxt\crypto\*.java src\java\nxt\util\*.java  src\java\nxt\http\*.java  src\java\fr\cryptohash\*.java 
+
+:/bin/rm -f burst.jar 
+:this seems to be overwritten by the compiler so not really any need to remove it on windows
+
+"C:\Program Files\Java\jdk1.7.0_67\bin\jar.exe" cf burst.jar -C classes .
+:/bin/rm -rf classes
+:seems to update fine even if not removed, so no immediate need to remove the directory
+
+echo "burst.jar generated successfully"
+pause
