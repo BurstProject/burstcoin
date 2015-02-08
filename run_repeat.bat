87b2cf1c6a4ac548b973f2ba8be4e3b9c0908b5d
Dennis Fantoni <dennis@fantoni.dk>
2015-02-08 01:16:12 +0100

    update from 1.2.1 to 1.2.2 using burstcoin. also updated run_repeat.bat to use newest java 8 version, and made it easier to adapt run_repeat.bat in case you update to an even newer version of java.

--- run_repeat.bat
+++ run_repeat.bat
@@ -1,16 +1,17 @@
 title BURST WALLET
 set MAXRAM=700m
 set LAUNCH=-Xmx%MAXRAM% -cp burst.jar;lib\*;conf nxt.Nxt
-set JAVAEXEC=\Java\jre7\bin\java.exe
+set JAVAVERSION=jre1.8.0_31
+set JAVAEXEC=\Java\%JAVAVERSION%\bin\java.exe
 :runagain
 @ECHO OFF
 IF EXIST java (
 	java %LAUNCH% 
 ) ELSE (
-	IF EXIST "%PROGRAMFILES%\Java\jre7" (
+	IF EXIST "%PROGRAMFILES%\Java\%JAVAVERSION%" (
 		"%PROGRAMFILES%%JAVAEXEC%" %LAUNCH% 
 	) ELSE (
-		IF EXIST "%PROGRAMFILES(X86)%\Java\jre7" (
+		IF EXIST "%PROGRAMFILES(X86)%\%JAVAVERSION%" (
 			"%PROGRAMFILES(X86)%%JAVAEXEC%" %LAUNCH% 
 		) ELSE (
 			ECHO Java software not found on your system. Please go to http://java.com/en/ to download a copy of Java.
