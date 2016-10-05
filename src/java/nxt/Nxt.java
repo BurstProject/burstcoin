250f5f2f844ef6900986894a0fea0d370a29870c
f <f@burst>
2016-10-05 17:24:13 -0400

    Updated version number to 1.2.7

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -18,7 +18,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "1.2.6";
+    public static final String VERSION = "1.2.7";
     public static final String APPLICATION = "NRS";
 
     private static volatile Time time = new Time.EpochTime();
