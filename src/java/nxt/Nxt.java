8c1681483b27956c1213ac3949857e3799012542
ac0v <ac0v@sys-network.de>
2017-07-25 14:19:54 +0200

    version bump

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -18,7 +18,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "1.2.8";
+    public static final String VERSION = "1.3.0cg";
     public static final String APPLICATION = "NRS";
 
     private static volatile Time time = new Time.EpochTime();
