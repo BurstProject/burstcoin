74a916d2bc2d9d1f1280a43d111220a0fe3221aa
BraindeadOne <Braindead.One@gmail.com>
2017-08-14 18:51:40 +0200

    Made AccountStore an Interface

--- src/java/nxt/db/store/AccountStore.java
+++ src/java/nxt/db/store/AccountStore.java
@@ -1,44 +1,41 @@
 package nxt.db.store;
 
 import nxt.Account;
-import nxt.Nxt;
 import nxt.db.NxtIterator;
 import nxt.db.VersionedBatchEntityTable;
 import nxt.db.VersionedEntityTable;
-import nxt.db.sql.DbClause;
-import nxt.db.sql.DbIterator;
 import nxt.db.sql.DbKey;
 
 /**
- * Base-Class for Database operations related to Accounts
+ * Interface for Database operations related to Accounts
  * Created by BraindeadOne on 10.08.2017.
  */
-public abstract class AccountStore {
+public interface AccountStore {
 
-    public abstract VersionedBatchEntityTable<Account> getAccountTable();
+    VersionedBatchEntityTable<Account> getAccountTable();
 
-    public abstract VersionedEntityTable<Account.RewardRecipientAssignment> getRewardRecipientAssignmentTable();
+    VersionedEntityTable<Account.RewardRecipientAssignment> getRewardRecipientAssignmentTable();
 
-    public abstract DbKey.LongKeyFactory<Account.RewardRecipientAssignment> getRewardRecipientAssignmentDbKeyFactory();
+    DbKey.LongKeyFactory<Account.RewardRecipientAssignment> getRewardRecipientAssignmentDbKeyFactory();
 
-    public abstract DbKey.LinkKeyFactory<Account.AccountAsset> getAccountAssetDbKeyFactory();
+    DbKey.LinkKeyFactory<Account.AccountAsset> getAccountAssetDbKeyFactory();
 
-    public abstract VersionedEntityTable<Account.AccountAsset> getAccountAssetTable();
+    VersionedEntityTable<Account.AccountAsset> getAccountAssetTable();
 
-    public abstract int getAssetAccountsCount(long assetId);
+    int getAssetAccountsCount(long assetId);
 
-    public abstract DbKey.LongKeyFactory<Account> getAccountDbKeyFactory();
+    DbKey.LongKeyFactory<Account> getAccountDbKeyFactory();
 
-    public abstract NxtIterator<Account.RewardRecipientAssignment> getAccountsWithRewardRecipient(Long recipientId);
+    NxtIterator<Account.RewardRecipientAssignment> getAccountsWithRewardRecipient(Long recipientId);
 
-    public abstract NxtIterator<Account.AccountAsset> getAssets(int from, int to, Long id);
+    NxtIterator<Account.AccountAsset> getAssets(int from, int to, Long id);
 
-    public abstract NxtIterator<Account.AccountAsset> getAssetAccounts(long assetId, int from, int to);
+    NxtIterator<Account.AccountAsset> getAssetAccounts(long assetId, int from, int to);
 
-    public abstract NxtIterator<Account.AccountAsset> getAssetAccounts(long assetId, int height, int from, int to);
+    NxtIterator<Account.AccountAsset> getAssetAccounts(long assetId, int height, int from, int to);
     // returns true iff:
     // this.publicKey is set to null (in which case this.publicKey also gets set to key)
     // or
     // this.publicKey is already set to an array equal to key
-    public abstract boolean setOrVerify(Account acc, byte[] key, int height);
+    boolean setOrVerify(Account acc, byte[] key, int height);
 }
