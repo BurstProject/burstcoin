0a69192f8d03d7f643234996207d3aee17d62fb2
BraindeadOne <Braindead.One@gmail.com>
2017-08-19 13:53:24 +0200

    Removed SQL from Asset, further refactoring to make that possible.

---
+++ src/java/nxt/db/mariadb/MariadbAssetStore.java
@@ -0,0 +1,6 @@
+package nxt.db.mariadb;
+
+import nxt.db.sql.SqlAssetStore;
+
+public class MariadbAssetStore extends SqlAssetStore {
+}
--- src/java/nxt/db/mariadb/MariadbStores.java
+++ src/java/nxt/db/mariadb/MariadbStores.java
@@ -1,5 +1,6 @@
 package nxt.db.mariadb;
 
+import nxt.db.sql.SqlAssetStore;
 import nxt.db.store.AccountStore;
 import nxt.db.store.AliasStore;
 import nxt.db.store.Stores;
@@ -8,10 +9,12 @@ public class MariadbStores implements Stores {
     private final AccountStore accountStore;
     private final AliasStore aliasStore;
 
+    private final SqlAssetStore assetStore;
+
     public MariadbStores() {
         this.accountStore = new MariadbAccountStore();
         this.aliasStore = new MariadbAliasStore();
-
+        this.assetStore = new MariadbAssetStore();
     }
 
     @Override
@@ -23,4 +26,10 @@ public class MariadbStores implements Stores {
     public AliasStore getAliasStore() {
         return aliasStore;
     }
+
+    @Override
+    public SqlAssetStore getAssetStore() {
+        return assetStore;
+    }
+
 }
