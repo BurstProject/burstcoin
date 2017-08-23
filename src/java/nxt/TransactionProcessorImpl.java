5bb1cdf95d95cd62d3407f25f144cd78f1b33421
BraindeadOne <Braindead.One@gmail.com>
2017-08-23 07:53:29 +0200

    Removed SQL from BlockchainProcessorImpl Disabled the scan method for the moment - its complicated to update and we are not sure if it works like it should
    Unfortunately the whitespace in this file was different from the other files and i noticed too late i changed most of it. Please add "?w=1" to the diff view to just see code changes (and some minor brace reformats)

--- src/java/nxt/TransactionProcessorImpl.java
+++ src/java/nxt/TransactionProcessorImpl.java
@@ -20,7 +20,7 @@ import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
-final class TransactionProcessorImpl implements TransactionProcessor {
+public final class TransactionProcessorImpl implements TransactionProcessor {
 
     private static final Logger logger = LoggerFactory.getLogger(TransactionProcessorImpl.class);
 
