47ed88d07543e5acda2192986914f522b0692a44
BraindeadOne <Braindead.One@gmail.com>
2017-09-03 17:00:13 +0200

    Replace into -> Replace|Merge into (ae080b780457e7373175ebbb19e7ee3872647eea)

--- src/java/nxt/db/h2/H2ATStore.java
+++ src/java/nxt/db/h2/H2ATStore.java
@@ -1,6 +1,32 @@
 package nxt.db.h2;
 
+import nxt.AT;
+import nxt.Nxt;
+import nxt.db.sql.DbUtils;
 import nxt.db.sql.SqlATStore;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 class H2ATStore extends SqlATStore {
+    @Override
+    protected void saveATState(Connection con, AT.ATState atState) throws SQLException {
+        try (PreparedStatement pstmt = con.prepareStatement("MERGE  INTO at_state (at_id, "
+                + "state, prev_height ,next_height, sleep_between, prev_balance, freeze_when_same_balance, min_activate_amount, height, latest) "
+                + "KEY (at_id, height)  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, TRUE)")) {
+            int i = 0;
+            pstmt.setLong(++i, atState.getATId());
+            //DbUtils.setBytes(pstmt, ++i, state);
+            DbUtils.setBytes(pstmt, ++i, AT.compressState(atState.getState()));
+            pstmt.setInt(++i, atState.getPrevHeight());
+            pstmt.setInt(++i, atState.getNextHeight());
+            pstmt.setInt(++i, atState.getSleepBetween());
+            pstmt.setLong(++i, atState.getPrevBalance());
+            pstmt.setBoolean(++i, atState.getFreezeWhenSameBalance());
+            pstmt.setLong(++i, atState.getMinActivationAmount());
+            pstmt.setInt(++i, Nxt.getBlockchain().getHeight());
+            pstmt.executeUpdate();
+        }
+    }
 }
--- src/java/nxt/db/sql/SqlEscrowStore.java
+++ src/java/nxt/db/sql/SqlEscrowStore.java
@@ -65,17 +65,7 @@ public abstract class SqlEscrowStore implements EscrowStore {
         };
     }
 
-    private void saveDecision(Connection con, Escrow.Decision decision) throws SQLException {
-        try (PreparedStatement pstmt = con.prepareStatement("REPLACE INTO escrow_decision (escrow_id, "
-                + "account_id, decision, height, latest) VALUES (?, ?, ?, ?, TRUE)")) {
-            int i = 0;
-            pstmt.setLong(++i, decision.escrowId);
-            pstmt.setLong(++i, decision.accountId);
-            pstmt.setInt(++i, Escrow.decisionToByte(decision.decision));
-            pstmt.setInt(++i, Nxt.getBlockchain().getHeight());
-            pstmt.executeUpdate();
-        }
-    }
+    protected abstract void saveDecision(Connection con, Escrow.Decision decision) throws SQLException;
 
     @Override
     public NxtKey.LongKeyFactory<Escrow> getEscrowDbKeyFactory() {
@@ -184,22 +174,7 @@ public abstract class SqlEscrowStore implements EscrowStore {
         escrowTable.delete(escrow);
     }
 
-    private void saveEscrow(Connection con, Escrow escrow) throws SQLException {
-        try (PreparedStatement pstmt = con.prepareStatement("REPLACE INTO escrow (id, sender_id, "
-                + "recipient_id, amount, required_signers, deadline, deadline_action, height, latest) "
-                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, TRUE)")) {
-            int i = 0;
-            pstmt.setLong(++i, escrow.id);
-            pstmt.setLong(++i, escrow.senderId);
-            pstmt.setLong(++i, escrow.recipientId);
-            pstmt.setLong(++i, escrow.amountNQT);
-            pstmt.setInt(++i, escrow.requiredSigners);
-            pstmt.setInt(++i, escrow.deadline);
-            pstmt.setInt(++i, Escrow.decisionToByte(escrow.deadlineAction));
-            pstmt.setInt(++i, Nxt.getBlockchain().getHeight());
-            pstmt.executeUpdate();
-        }
-    }
+    protected abstract void saveEscrow(Connection con, Escrow escrow) throws SQLException;
 
     private class SqlDecision extends Escrow.Decision {
         private SqlDecision(ResultSet rs) throws SQLException {
