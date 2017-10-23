23be7d93bc38dda30f850887f943a238c6640caa
ac0v <ac0v@sys-network.de>
2017-10-23 21:25:07 +0200

    created command aggregator burst.cmd which replaces: createBinDump.bat, loadBinDump.bat, run.bat

---
+++ burst.cmd
@@ -0,0 +1,43 @@
+@ECHO OFF
+
+SET MY_SELF=%0
+SET MY_CMD=%1
+SET MY_OPT=%2
+
+IF "%MY_JAVA%" == "" (
+  for /F "tokens=*" %%f in ('where /F /R %SYSTEMDRIVE% java.exe') do (
+      SET MY_JAVA=%%f
+  )
+)
+
+IF "%MY_JAVA%" == "" (
+    echo please install java from eg. https://java.com/de/download/
+    goto DONE
+)
+
+IF NOT "%MY_CMD%" == "" (
+    IF "%MY_OPT%" == "" (
+        goto USAGE
+    )
+    IF "%MY_CMD%" == "load" (
+        %MY_JAVA% -cp burst.jar;conf nxt.db.quicksync.LoadBinDump "$MY_OPT"
+        goto DONE
+    )
+    IF "%MY_CMD%" == "dump" (
+        %MY_JAVA% -cp burst.jar;conf nxt.db.quicksync.CreateBinDump "$MY_OPT"
+        goto DONE
+    )
+    goto USAGE
+)
+%MY_JAVA% -cp burst.jar;conf nxt.Nxt
+
+goto DONE
+
+:USAGE
+
+echo usage: %MY_SELF% [command] [arguments]
+echo   load [filename or url]  quick import by loading a binary dump
+echo   dump [filename]         create a binary dump usable for doing a quick import
+echo   help                    shows the help you just read
+
+:DONE
