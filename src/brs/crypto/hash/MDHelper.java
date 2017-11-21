84f47dfb164a232c9866634f102a1ff56ad75bac
rico666 <bots@cryptoguru.org>
2017-11-21 17:21:26 +0100

    further cleanup in brs.crypto; RIPEMD(128) not used at all; SHA256 not used from brs.crypto.hash but from java.security.MessageDigest instead

--- src/brs/crypto/hash/MDHelper.java
+++ src/brs/crypto/hash/MDHelper.java
@@ -38,115 +38,115 @@ package brs.crypto.hash;
 
 abstract class MDHelper extends DigestEngine {
 
-	/**
-	 * Create the object. Little-endian padding is for MD4, MD5 and
-	 * RIPEMD-160; the SHA family uses big-endian padding. The
-	 * MD padding includes an encoding of the input message bit length,
-	 * which is over 64 bits for some algorithms, 128-bit for others
-	 * (namely SHA-384 and SHA-512). Note that this implementation
-	 * handles only message lengths which fit on 64 bits.
-	 *
-	 * @param littleEndian   {@code true} for little-endian padding
-	 * @param lenlen         the length encoding length, in bytes (must
-	 *                       be at least 8)
-	 */
-	MDHelper(boolean littleEndian, int lenlen)
-	{
-		this(littleEndian, lenlen, (byte)0x80);
-	}
+  /**
+   * Create the object. Little-endian padding is for MD4, MD5 and
+   * RIPEMD-160; the SHA family uses big-endian padding. The
+   * MD padding includes an encoding of the input message bit length,
+   * which is over 64 bits for some algorithms, 128-bit for others
+   * (namely SHA-384 and SHA-512). Note that this implementation
+   * handles only message lengths which fit on 64 bits.
+   *
+   * @param littleEndian   {@code true} for little-endian padding
+   * @param lenlen         the length encoding length, in bytes (must
+   *                       be at least 8)
+   */
+  MDHelper(boolean littleEndian, int lenlen)
+  {
+    this(littleEndian, lenlen, (byte)0x80);
+  }
 
-	/**
-	 * Create the object. Little-endian padding is for MD4, MD5 and
-	 * RIPEMD-160; the SHA family uses big-endian padding. The
-	 * MD padding includes an encoding of the input message bit length,
-	 * which is over 64 bits for some algorithms, 128-bit for others
-	 * (namely SHA-384 and SHA-512). Note that this implementation
-	 * handles only message lengths which fit on 64 bits. The first
-	 * additional byte value is specified; this is normally 0x80,
-	 * except for Tiger (not Tiger2) which uses 0x01.
-	 *
-	 * @param littleEndian   {@code true} for little-endian padding
-	 * @param lenlen         the length encoding length, in bytes (must
-	 *                       be at least 8)
-	 * @param fbyte          the first padding byte
-	 */
-	MDHelper(boolean littleEndian, int lenlen, byte fbyte)
-	{
-		this.littleEndian = littleEndian;
-		countBuf = new byte[lenlen];
-		this.fbyte = fbyte;
-	}
+  /**
+   * Create the object. Little-endian padding is for MD4, MD5 and
+   * RIPEMD-160; the SHA family uses big-endian padding. The
+   * MD padding includes an encoding of the input message bit length,
+   * which is over 64 bits for some algorithms, 128-bit for others
+   * (namely SHA-384 and SHA-512). Note that this implementation
+   * handles only message lengths which fit on 64 bits. The first
+   * additional byte value is specified; this is normally 0x80,
+   * except for Tiger (not Tiger2) which uses 0x01.
+   *
+   * @param littleEndian   {@code true} for little-endian padding
+   * @param lenlen         the length encoding length, in bytes (must
+   *                       be at least 8)
+   * @param fbyte          the first padding byte
+   */
+  MDHelper(boolean littleEndian, int lenlen, byte fbyte)
+  {
+    this.littleEndian = littleEndian;
+    countBuf = new byte[lenlen];
+    this.fbyte = fbyte;
+  }
 
-	private boolean littleEndian;
-	private byte[] countBuf;
-	private byte fbyte;
+  private boolean littleEndian;
+  private byte[] countBuf;
+  private byte fbyte;
 
-	/**
-	 * Compute the padding. The padding data is input into the engine,
-	 * which is flushed.
-	 */
-	protected void makeMDPadding()
-	{
-		int dataLen = flush();
-		int blen = getBlockLength();
-		long currentLength = getBlockCount() * (long)blen;
-		currentLength = (currentLength + (long)dataLen) * 8L;
-		int lenlen = countBuf.length;
-		if (littleEndian) {
-			encodeLEInt((int)currentLength, countBuf, 0);
-			encodeLEInt((int)(currentLength >>> 32), countBuf, 4);
-		} else {
-			encodeBEInt((int)(currentLength >>> 32),
-				countBuf, lenlen - 8);
-			encodeBEInt((int)currentLength,
-				countBuf, lenlen - 4);
-		}
-		int endLen = (dataLen + lenlen + blen) & ~(blen - 1);
-		update(fbyte);
-		for (int i = dataLen + 1; i < endLen - lenlen; i ++)
-			update((byte)0);
-		update(countBuf);
+  /**
+   * Compute the padding. The padding data is input into the engine,
+   * which is flushed.
+   */
+  protected void makeMDPadding()
+  {
+    int dataLen = flush();
+    int blen = getBlockLength();
+    long currentLength = getBlockCount() * (long)blen;
+    currentLength = (currentLength + (long)dataLen) * 8L;
+    int lenlen = countBuf.length;
+    if (littleEndian) {
+      encodeLEInt((int)currentLength, countBuf, 0);
+      encodeLEInt((int)(currentLength >>> 32), countBuf, 4);
+    } else {
+      encodeBEInt((int)(currentLength >>> 32),
+                  countBuf, lenlen - 8);
+      encodeBEInt((int)currentLength,
+                  countBuf, lenlen - 4);
+    }
+    int endLen = (dataLen + lenlen + blen) & ~(blen - 1);
+    update(fbyte);
+    for (int i = dataLen + 1; i < endLen - lenlen; i ++)
+      update((byte)0);
+    update(countBuf);
 
-		/*
-		 * This code is used only for debugging purposes.
-		 *
-		if (flush() != 0)
-			throw new Error("panic: buffering went astray");
-		 *
-		 */
-	}
+    /*
+     * This code is used only for debugging purposes.
+     *
+     if (flush() != 0)
+     throw new Error("panic: buffering went astray");
+     *
+     */
+  }
 
-	/**
-	 * Encode the 32-bit word {@code val} into the array
-	 * {@code buf} at offset {@code off}, in little-endian
-	 * convention (least significant byte first).
-	 *
-	 * @param val   the value to encode
-	 * @param buf   the destination buffer
-	 * @param off   the destination offset
-	 */
-	private static final void encodeLEInt(int val, byte[] buf, int off)
-	{
-		buf[off + 0] = (byte)val;
-		buf[off + 1] = (byte)(val >>> 8);
-		buf[off + 2] = (byte)(val >>> 16);
-		buf[off + 3] = (byte)(val >>> 24);
-	}
+  /**
+   * Encode the 32-bit word {@code val} into the array
+   * {@code buf} at offset {@code off}, in little-endian
+   * convention (least significant byte first).
+   *
+   * @param val   the value to encode
+   * @param buf   the destination buffer
+   * @param off   the destination offset
+   */
+  private static final void encodeLEInt(int val, byte[] buf, int off)
+  {
+    buf[off + 0] = (byte)val;
+    buf[off + 1] = (byte)(val >>> 8);
+    buf[off + 2] = (byte)(val >>> 16);
+    buf[off + 3] = (byte)(val >>> 24);
+  }
 
-	/**
-	 * Encode the 32-bit word {@code val} into the array
-	 * {@code buf} at offset {@code off}, in big-endian
-	 * convention (most significant byte first).
-	 *
-	 * @param val   the value to encode
-	 * @param buf   the destination buffer
-	 * @param off   the destination offset
-	 */
-	private static final void encodeBEInt(int val, byte[] buf, int off)
-	{
-		buf[off + 0] = (byte)(val >>> 24);
-		buf[off + 1] = (byte)(val >>> 16);
-		buf[off + 2] = (byte)(val >>> 8);
-		buf[off + 3] = (byte)val;
-	}
+  /**
+   * Encode the 32-bit word {@code val} into the array
+   * {@code buf} at offset {@code off}, in big-endian
+   * convention (most significant byte first).
+   *
+   * @param val   the value to encode
+   * @param buf   the destination buffer
+   * @param off   the destination offset
+   */
+  private static final void encodeBEInt(int val, byte[] buf, int off)
+  {
+    buf[off + 0] = (byte)(val >>> 24);
+    buf[off + 1] = (byte)(val >>> 16);
+    buf[off + 2] = (byte)(val >>> 8);
+    buf[off + 3] = (byte)val;
+  }
 }
