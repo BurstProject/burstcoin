d93190a7e542a5d51b5be56ec0744603a17b763e
Andreas 'ac0v' Specht <ac0v@sys-network.de>
2017-11-28 17:05:19 +0100

    fixed formatting

--- src/brs/DigitalGoodsStore.java
+++ src/brs/DigitalGoodsStore.java
@@ -521,7 +521,7 @@ public final class DigitalGoodsStore {
   static void deliver(Transaction transaction, Attachment.DigitalGoodsDelivery attachment) {
     Purchase purchase = getPendingPurchase(attachment.getPurchaseId());
     if ( purchase == null ) {
-      throw new RuntimeException("cant find purchase with id {}", attachment.getPurchaseId());
+      throw new RuntimeException("cant find purchase with id " + attachment.getPurchaseId());
     }
     purchase.setPending(false);
     long totalWithoutDiscount = Convert.safeMultiply(purchase.getQuantity(), purchase.getPriceNQT());
