d2810f3b06b33ca6744a43c3d2d7551e74d3ed14
Brabantian <brabantianburst@gmail.com>
2018-01-07 11:20:04 +0100

    Added Powermockito; More tests; Refactoring example

---
+++ test/java/brs/common/TestConstants.java
@@ -0,0 +1,9 @@
+package brs.common;
+
+public class TestConstants {
+
+  public static final String TEST_ACCOUNT_ID = "BURST-2222-2222-2222-22222";
+
+  public static final long TEST_ACCOUNT_ID_PARSED = 0L;
+
+}
---
+++ test/java/brs/http/AbstractCreateTransactionTest.java
@@ -0,0 +1,5 @@
+package brs.http;
+
+public class AbstractCreateTransactionTest {
+
+}
