99b961a1df9d0c9872a1be1fa30ac99f48653240
Brabantian <brabantianburst@gmail.com>
2018-01-07 11:20:04 +0100

    Added Powermockito; More tests; Refactoring example

---
+++ test/java/brs/http/BroadcastTransactionTest.java
@@ -0,0 +1,102 @@
+package brs.http;
+
+import static brs.http.BroadcastTransaction.ERROR_CODE_RESPONSE_FIELD;
+import static brs.http.BroadcastTransaction.ERROR_DESCRIPTION_RESPONSE_FIELD;
+import static brs.http.BroadcastTransaction.FULL_HASH_RESPONSE_FIELD;
+import static brs.http.BroadcastTransaction.TRANSACTION_BYTES_PARAMETER_FIELD;
+import static brs.http.BroadcastTransaction.TRANSACTION_JSON_PARAMETER_FIELD;
+import static brs.http.BroadcastTransaction.TRANSACTION_RESPONSE_FIELD;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import brs.BurstException;
+import brs.DIContainer;
+import brs.Transaction;
+import brs.TransactionProcessor;
+import javax.servlet.http.HttpServletRequest;
+import org.json.simple.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({DIContainer.class, ParameterParser.class})
+public class BroadcastTransactionTest {
+
+  private BroadcastTransaction t;
+
+  @Before
+  public void setUp() {
+    t = BroadcastTransaction.instance;
+  }
+
+  @Test
+  public void processRequest() throws BurstException {
+    PowerMockito.mockStatic(ParameterParser.class);
+    PowerMockito.mockStatic(DIContainer.class);
+
+    final String mockTransactionBytesParameter = "mockTransactionBytesParameter";
+    final String mockTransactionJson = "mockTransactionJson";
+
+    final String mockTransactionStringId = "mockTransactionStringId";
+    final String mockTransactionFullHash = "mockTransactionFullHash";
+
+    final HttpServletRequest req = mock(HttpServletRequest.class);
+    final Transaction mockTransaction = mock(Transaction.class);
+    final TransactionProcessor mockTransactionProcessor = mock(TransactionProcessor.class);
+
+    when(mockTransaction.getStringId()).thenReturn(mockTransactionStringId);
+    when(mockTransaction.getFullHash()).thenReturn(mockTransactionFullHash);
+
+    when(req.getParameter(TRANSACTION_BYTES_PARAMETER_FIELD)).thenReturn(mockTransactionBytesParameter);
+    when(req.getParameter(TRANSACTION_JSON_PARAMETER_FIELD)).thenReturn(mockTransactionJson);
+
+    when(ParameterParser.parseTransaction(eq(mockTransactionBytesParameter), eq(mockTransactionJson))).thenReturn(mockTransaction);
+
+    when(DIContainer.getTransactionProcessor()).thenReturn(mockTransactionProcessor);
+
+    final JSONObject result = (JSONObject) t.processRequest(req);
+
+    verify(mockTransactionProcessor).broadcast(eq(mockTransaction));
+
+    assertEquals(mockTransactionStringId, result.get(TRANSACTION_RESPONSE_FIELD));
+    assertEquals(mockTransactionFullHash, result.get(FULL_HASH_RESPONSE_FIELD));
+  }
+
+  @Test
+  public void processRequest_validationException() throws BurstException {
+    PowerMockito.mockStatic(ParameterParser.class);
+
+    final String mockTransactionBytesParameter = "mockTransactionBytesParameter";
+    final String mockTransactionJson = "mockTransactionJson";
+
+    final HttpServletRequest req = mock(HttpServletRequest.class);
+    final Transaction mockTransaction = mock(Transaction.class);
+
+    when(req.getParameter(TRANSACTION_BYTES_PARAMETER_FIELD)).thenReturn(mockTransactionBytesParameter);
+    when(req.getParameter(TRANSACTION_JSON_PARAMETER_FIELD)).thenReturn(mockTransactionJson);
+
+    when(ParameterParser.parseTransaction(eq(mockTransactionBytesParameter), eq(mockTransactionJson))).thenReturn(mockTransaction);
+
+    Mockito.doThrow(BurstException.NotCurrentlyValidException.class).when(mockTransaction).validate();
+
+    final JSONObject result = (JSONObject) t.processRequest(req);
+
+    assertEquals(4, result.get(ERROR_CODE_RESPONSE_FIELD));
+    assertNotNull(result.get(ERROR_DESCRIPTION_RESPONSE_FIELD));
+  }
+
+  @Test
+  public void requirePost() {
+    assertTrue(t.requirePost());
+  }
+}
---
+++ test/java/brs/peer/GetAccountBalanceTest.java
@@ -0,0 +1,65 @@
+package brs.peer;
+
+import static brs.common.TestConstants.TEST_ACCOUNT_ID;
+import static brs.common.TestConstants.TEST_ACCOUNT_ID_PARSED;
+import static brs.peer.GetAccountBalance.ACCOUNT_ID_PARAMETER_FIELD;
+import static brs.peer.GetAccountBalance.BALANCE_NQT_RESPONSE_FIELD;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import brs.Account;
+import org.json.simple.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Account.class)
+public class GetAccountBalanceTest {
+
+  private GetAccountBalance t;
+
+  @Before
+  public void setUp() {
+    t = GetAccountBalance.instance;
+  }
+
+  @Test
+  public void processRequest() {
+    PowerMockito.mockStatic(Account.class);
+
+    final JSONObject req = new JSONObject();
+    req.put(ACCOUNT_ID_PARAMETER_FIELD, TEST_ACCOUNT_ID);
+    final Peer peer = mock(Peer.class);
+
+    long mockBalanceNQT = 5;
+    Account mockAccount = mock(Account.class);
+    when(mockAccount.getBalanceNQT()).thenReturn(mockBalanceNQT);
+
+    when(Account.getAccount(eq(TEST_ACCOUNT_ID_PARSED))).thenReturn(mockAccount);
+
+    final JSONObject result = (JSONObject) t.processRequest(req, peer);
+
+    assertEquals("" + mockBalanceNQT, result.get(BALANCE_NQT_RESPONSE_FIELD));
+  }
+
+  @Test
+  public void processRequest_notExistingAccount() {
+    PowerMockito.mockStatic(Account.class);
+
+    final JSONObject req = new JSONObject();
+    req.put(ACCOUNT_ID_PARAMETER_FIELD, TEST_ACCOUNT_ID);
+    final Peer peer = mock(Peer.class);
+
+    when(Account.getAccount(eq(TEST_ACCOUNT_ID_PARSED))).thenReturn(null);
+
+    final JSONObject result = (JSONObject) t.processRequest(req, peer);
+
+    assertEquals("0", result.get(BALANCE_NQT_RESPONSE_FIELD));
+  }
+}
