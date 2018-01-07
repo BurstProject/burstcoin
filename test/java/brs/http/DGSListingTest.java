ebe85e5a2ba9f24f4ccc941a71112bf0da9d81ee
Brabantian <brabantianburst@gmail.com>
2018-01-07 16:30:54 +0100

    Some more test TODO's

---
+++ test/java/brs/http/DGSListingTest.java
@@ -0,0 +1,11 @@
+package brs.http;
+
+import org.junit.Test;
+
+public class DGSListingTest {
+
+  @Test
+  public void processRequest() {
+    //TODO Add tests
+  }
+}
---
+++ test/java/brs/http/DGSQuantityChangeTest.java
@@ -0,0 +1,11 @@
+package brs.http;
+
+import org.junit.Test;
+
+public class DGSQuantityChangeTest {
+
+  @Test
+  public void processRequest() {
+    //TODO Add tests
+  }
+}
