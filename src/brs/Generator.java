1565dd0216215e997d8e8db2960e8e1bd5990a74
Quibus-burst <31744573+Quibus-burst@users.noreply.github.com>
2018-01-10 21:26:40 +0100

    first PoC2 Concept

--- src/brs/Generator.java
+++ src/brs/Generator.java
@@ -1,9 +1,9 @@
 package brs;
 
-import brs.util.Listener;
-
 import java.math.BigInteger;
 import java.util.Collection;
+
+import brs.util.Listener;
 
 public interface Generator {
 
@@ -12,22 +12,34 @@ public interface Generator {
   }
 
   void init();
+
   boolean addListener(Listener<GeneratorState> listener, Event eventType);
+
   boolean removeListener(Listener<GeneratorState> listener, Event eventType);
+
   GeneratorState addNonce(String secretPhrase, Long nonce);
+
   GeneratorState addNonce(String secretPhrase, Long nonce, byte[] publicKey);
+
   Collection<? extends GeneratorState> getAllGenerators();
 
   byte[] calculateGenerationSignature(byte[] lastGenSig, long lastGenId);
+
   int calculateScoop(byte[] genSig, long height);
-  BigInteger calculateHit(long accountId, long nonce, byte[] genSig, int scoop);
+
+  BigInteger calculateHit(long accountId, long nonce, byte[] genSig, int scoop, int blockHeight);
+
   BigInteger calculateHit(long accountId, long nonce, byte[] genSig, byte[] scoopData);
-  BigInteger calculateDeadline(long accountId, long nonce, byte[] genSig, int scoop, long baseTarget);
+
+  BigInteger calculateDeadline(long accountId, long nonce, byte[] genSig, int scoop, long baseTarget, int blockHeight);
 
   interface GeneratorState {
     byte[] getPublicKey();
+
     Long getAccountId();
+
     BigInteger getDeadline();
+
     long getBlock();
   }
 }
--- src/brs/util/MiningPlot.java
+++ src/brs/util/MiningPlot.java
@@ -1,9 +1,10 @@
 package brs.util;
 
-import brs.crypto.hash.Shabal256;
-
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+
+import brs.Constants;
+import brs.crypto.hash.Shabal256;
 
 public class MiningPlot {
   public static final int HASH_SIZE = 32;
@@ -11,12 +12,12 @@ public class MiningPlot {
   public static final int SCOOP_SIZE = HASHES_PER_SCOOP * HASH_SIZE;
   public static final int SCOOPS_PER_PLOT = 4096; // original 1MB/plot = 16384
   public static final int PLOT_SIZE = SCOOPS_PER_PLOT * SCOOP_SIZE;
-	
+
   public static final int HASH_CAP = 4096;
-	
+
   private byte[] data = new byte[PLOT_SIZE];
-	
-  public MiningPlot(long addr, long nonce) {
+
+  public MiningPlot(long addr, long nonce, int blockHeight) {
     ByteBuffer base_buffer = ByteBuffer.allocate(16);
     base_buffer.putLong(addr);
     base_buffer.putLong(nonce);
@@ -39,12 +40,23 @@ public class MiningPlot {
     for (int i = 0; i < PLOT_SIZE; i++) {
       data[i] = (byte) (gendata[i] ^ finalhash[i % HASH_SIZE]);
     }
+    //PoC2 Rearrangement
+    if (blockHeight >= Constants.POC2_START_BLOCK) {
+      byte[] hashBuffer = new byte[HASH_SIZE];
+      int revPos = PLOT_SIZE - HASH_SIZE; //Start at second hash in last scoop
+      for (int pos = 32; pos < (PLOT_SIZE / 2); pos += 64) { //Start at second hash in first scoop
+        System.arraycopy(data, pos, hashBuffer, 0, HASH_SIZE); //Copy low scoop second hash to buffer
+        System.arraycopy(data, revPos, data, pos, HASH_SIZE); //Copy high scoop second hash to low scoop second hash
+        System.arraycopy(hashBuffer, 0, data, revPos, HASH_SIZE); //Copy buffer to high scoop second hash
+        revPos -= 64; //move backwards
+      }
+    }
   }
-	
+
   public byte[] getScoop(int pos) {
     return Arrays.copyOfRange(data, pos * SCOOP_SIZE, (pos + 1) * SCOOP_SIZE);
   }
-	
+
   public void hashScoop(Shabal256 md, int pos) {
     md.update(data, pos * SCOOP_SIZE, SCOOP_SIZE);
   }
