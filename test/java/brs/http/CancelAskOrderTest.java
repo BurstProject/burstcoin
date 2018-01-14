545fd5588570486be0b60e10460570a8d0d6669f
Brabantian <brabantianburst@gmail.com>
2018-01-14 17:32:01 +0100

    Correct number of arguments

--- test/java/brs/http/CancelAskOrderTest.java
+++ test/java/brs/http/CancelAskOrderTest.java
@@ -5,11 +5,12 @@ import static org.powermock.api.mockito.PowerMockito.mock;
 import brs.Blockchain;
 import brs.TransactionProcessor;
 import brs.services.AccountService;
+import brs.services.OrderService;
 import brs.services.ParameterService;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CancelAskOrderTest {
+public class CancelAskOrderTest extends AbstractTransactionTest {
 
   private CancelAskOrder t;
 
@@ -17,6 +18,7 @@ public class CancelAskOrderTest {
   private TransactionProcessor transactionProcessorMock;
   private Blockchain blockchainMock;
   private AccountService accountServiceMock;
+  private OrderService orderServiceMock;
 
   @Before
   public void setUp() {
@@ -24,44 +26,14 @@ public class CancelAskOrderTest {
     transactionProcessorMock = mock(TransactionProcessor.class);
     blockchainMock = mock(Blockchain.class);
     accountServiceMock = mock(AccountService.class);
+    orderServiceMock = mock(OrderService.class);
 
-    t = new CancelAskOrder(parameterServiceMock, transactionProcessorMock, blockchainMock, accountServiceMock);
+    t = new CancelAskOrder(parameterServiceMock, transactionProcessorMock, blockchainMock, accountServiceMock, orderServiceMock);
   }
 
   @Test
   public void processRequest() {
     //TODO Add tests
   }
-
-  /*
-  @Test
-  public void processRequest_unknownOrderDueNoOrderData() throws BurstException {
-    System.out.println(getHex(Crypto.getPublicKey(TestConstants.TEST_SECRET_PHRASE)));
-
-    final HttpServletRequest req = QuickMocker.httpServletRequestDefaultKeys(
-        new MockParam(ORDER_PARAMETER, "3"));
-
-    assertEquals(UNKNOWN_ORDER, t.processRequest(req));
-  }
-  */
-
-  private static final String HEXES = "0123456789ABCDEF";
-
-  static String getHex(byte[] raw) {
-    final StringBuilder hex = new StringBuilder(2 * raw.length);
-    for (final byte b : raw) {
-      hex.append(HEXES.charAt((b & 0xF0) >> 4)).append(HEXES.charAt((b & 0x0F)));
-    }
-    return hex.toString();
-  }
-/*
-  @Test
-  public void processRequest_unknownOrderDueWrongAccountId() throws BurstException {
-    final HttpServletRequest req = QuickMocker.httpServletRequestDefaultKeys(
-        new MockParam(ORDER_PARAMETER, "3"));
-
-    assertEquals(UNKNOWN_ORDER, t.processRequest(req));
-  }
-*/
 
 }
