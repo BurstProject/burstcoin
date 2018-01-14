2477883f812e43f57dc3de4d8109bd4bdec85476
rico666 <bots@cryptoguru.org>
2018-01-14 17:33:12 +0100

    fixed a test

--- test/java/brs/http/CancelAskOrderTest.java
+++ test/java/brs/http/CancelAskOrderTest.java
@@ -5,6 +5,7 @@ import static org.powermock.api.mockito.PowerMockito.mock;
 import brs.Blockchain;
 import brs.TransactionProcessor;
 import brs.services.AccountService;
+import brs.services.OrderService;
 import brs.services.ParameterService;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +18,7 @@ public class CancelAskOrderTest {
   private TransactionProcessor transactionProcessorMock;
   private Blockchain blockchainMock;
   private AccountService accountServiceMock;
+  private OrderService orderServiceMock;
 
   @Before
   public void setUp() {
@@ -24,8 +26,9 @@ public class CancelAskOrderTest {
     transactionProcessorMock = mock(TransactionProcessor.class);
     blockchainMock = mock(Blockchain.class);
     accountServiceMock = mock(AccountService.class);
+    orderServiceMock = mock(OrderService.class);
 
-    t = new CancelAskOrder(parameterServiceMock, transactionProcessorMock, blockchainMock, accountServiceMock);
+    t = new CancelAskOrder(parameterServiceMock, transactionProcessorMock, blockchainMock, accountServiceMock, orderServiceMock);
   }
 
   @Test
