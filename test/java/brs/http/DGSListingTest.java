a23b782e41709414adfcca09154e7b87521faae1
Brabantian <brabantianburst@gmail.com>
2018-01-23 18:45:30 +0100

    DGSListingTest, DGSPriceChangeTest, DGSQuantityChangeTest, DGSRefundTest

--- test/java/brs/http/DGSListingTest.java
+++ test/java/brs/http/DGSListingTest.java
@@ -1,11 +1,131 @@
 package brs.http;
 
+import static brs.http.JSONResponses.INCORRECT_DGS_LISTING_DESCRIPTION;
+import static brs.http.JSONResponses.INCORRECT_DGS_LISTING_NAME;
+import static brs.http.JSONResponses.INCORRECT_DGS_LISTING_TAGS;
+import static brs.http.JSONResponses.MISSING_NAME;
+import static brs.http.common.Parameters.DESCRIPTION_PARAMETER;
+import static brs.http.common.Parameters.NAME_PARAMETER;
+import static brs.http.common.Parameters.PRICE_NQT_PARAMETER;
+import static brs.http.common.Parameters.QUANTITY_PARAMETER;
+import static brs.http.common.Parameters.TAGS_PARAMETER;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import brs.Account;
+import brs.Blockchain;
+import brs.BurstException;
+import brs.TransactionProcessor;
+import brs.common.QuickMocker;
+import brs.common.QuickMocker.MockParam;
+import brs.services.AccountService;
+import brs.services.ParameterService;
+import javax.servlet.http.HttpServletRequest;
+import org.junit.Before;
 import org.junit.Test;
 
-public class DGSListingTest {
+public class DGSListingTest extends AbstractTransactionTest {
+
+  private DGSListing t;
+
+  private ParameterService mockParameterService;
+  private Blockchain mockBlockchain;
+  private TransactionProcessor mockTransactionProcessor;
+  private AccountService mockAccountService;
+
+  @Before
+  public void setUp() {
+    mockParameterService = mock(ParameterService.class);
+    mockBlockchain = mock(Blockchain.class);
+    mockTransactionProcessor = mock(TransactionProcessor.class);
+    mockAccountService = mock(AccountService.class);
+
+    t = new DGSListing(mockParameterService, mockTransactionProcessor, mockBlockchain, mockAccountService);
+  }
 
   @Test
-  public void processRequest() {
-    //TODO Add tests
+  public void processRequest() throws BurstException {
+    final Account mockAccount = mock(Account.class);
+
+    final HttpServletRequest req = QuickMocker.httpServletRequest(
+        new MockParam(PRICE_NQT_PARAMETER, 123),
+        new MockParam(QUANTITY_PARAMETER, 1),
+        new MockParam(NAME_PARAMETER, "name"),
+        new MockParam(DESCRIPTION_PARAMETER, "description"),
+        new MockParam(TAGS_PARAMETER, "tags")
+    );
+
+    when(mockParameterService.getSenderAccount(eq(req))).thenReturn(mockAccount);
+
+    prepareTransactionTest(req, mockParameterService, mockTransactionProcessor, mockAccount);
+
+    t.processRequest(req);
   }
+
+  @Test
+  public void processRequest_missingName() throws BurstException {
+    final HttpServletRequest req = QuickMocker.httpServletRequest(
+        new MockParam(PRICE_NQT_PARAMETER, 123),
+        new MockParam(QUANTITY_PARAMETER, 1)
+    );
+
+    assertEquals(MISSING_NAME, t.processRequest(req));
+  }
+
+  @Test
+  public void processRequest_incorrectDGSListingName() throws BurstException {
+    String tooLongName = "";
+
+    for (int i = 0; i < 101; i++) {
+      tooLongName += "a";
+    }
+
+    final HttpServletRequest req = QuickMocker.httpServletRequest(
+        new MockParam(PRICE_NQT_PARAMETER, 123),
+        new MockParam(QUANTITY_PARAMETER, 1),
+        new MockParam(NAME_PARAMETER, tooLongName)
+    );
+
+    assertEquals(INCORRECT_DGS_LISTING_NAME, t.processRequest(req));
+  }
+
+  @Test
+  public void processRequest_incorrectDgsListingDescription() throws BurstException {
+    String tooLongDescription = "";
+
+    for (int i = 0; i < 1001; i++) {
+      tooLongDescription += "a";
+    }
+
+    final HttpServletRequest req = QuickMocker.httpServletRequest(
+        new MockParam(PRICE_NQT_PARAMETER, 123),
+        new MockParam(QUANTITY_PARAMETER, 1),
+        new MockParam(NAME_PARAMETER, "name"),
+        new MockParam(DESCRIPTION_PARAMETER, tooLongDescription)
+    );
+
+    assertEquals(INCORRECT_DGS_LISTING_DESCRIPTION, t.processRequest(req));
+  }
+
+  @Test
+  public void processRequest_incorrectDgsListingTags() throws BurstException {
+    String tooLongTags = "";
+
+    for (int i = 0; i < 101; i++) {
+      tooLongTags += "a";
+    }
+
+    final HttpServletRequest req = QuickMocker.httpServletRequest(
+        new MockParam(PRICE_NQT_PARAMETER, 123),
+        new MockParam(QUANTITY_PARAMETER, 1),
+        new MockParam(NAME_PARAMETER, "name"),
+        new MockParam(DESCRIPTION_PARAMETER, "description"),
+        new MockParam(TAGS_PARAMETER, tooLongTags)
+    );
+
+    assertEquals(INCORRECT_DGS_LISTING_TAGS, t.processRequest(req));
+  }
+
 }
--- test/java/brs/http/DGSQuantityChangeTest.java
+++ test/java/brs/http/DGSQuantityChangeTest.java
@@ -1,11 +1,173 @@
 package brs.http;
 
+import static brs.http.JSONResponses.INCORRECT_DELTA_QUANTITY;
+import static brs.http.JSONResponses.MISSING_DELTA_QUANTITY;
+import static brs.http.JSONResponses.UNKNOWN_GOODS;
+import static brs.http.common.Parameters.DELTA_QUALITY_PARAMETER;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import brs.Account;
+import brs.Blockchain;
+import brs.BurstException;
+import brs.DigitalGoodsStore.Goods;
+import brs.TransactionProcessor;
+import brs.common.QuickMocker;
+import brs.common.QuickMocker.MockParam;
+import brs.services.AccountService;
+import brs.services.ParameterService;
+import javax.servlet.http.HttpServletRequest;
+import org.junit.Before;
 import org.junit.Test;
 
-public class DGSQuantityChangeTest {
+public class DGSQuantityChangeTest extends AbstractTransactionTest {
+
+  private DGSQuantityChange t;
+
+  private ParameterService mockParameterService;
+  private Blockchain mockBlockchain;
+  private TransactionProcessor mockTransactionProcessor;
+  private AccountService mockAccountService;
+
+  @Before
+  public void setUp() {
+    mockParameterService = mock(ParameterService.class);
+    mockBlockchain = mock(Blockchain.class);
+    mockTransactionProcessor = mock(TransactionProcessor.class);
+    mockAccountService = mock(AccountService.class);
+
+    t = new DGSQuantityChange(mockParameterService, mockTransactionProcessor, mockBlockchain, mockAccountService);
+  }
 
   @Test
-  public void processRequest() {
-    //TODO Add tests
+  public void processRequest() throws BurstException {
+    final HttpServletRequest req = QuickMocker.httpServletRequest(
+        new MockParam(DELTA_QUALITY_PARAMETER, 10)
+    );
+
+    final Goods mockGoods = mock(Goods.class);
+    when(mockGoods.isDelisted()).thenReturn(false);
+    when(mockGoods.getSellerId()).thenReturn(1L);
+
+    final Account mockSenderAccount = mock(Account.class);
+    when(mockSenderAccount.getId()).thenReturn(1L);
+
+    when(mockParameterService.getSenderAccount(eq(req))).thenReturn(mockSenderAccount);
+    when(mockParameterService.getGoods(eq(req))).thenReturn(mockGoods);
+
+    prepareTransactionTest(req, mockParameterService, mockTransactionProcessor);
+
+    t.processRequest(req);
   }
+
+  @Test
+  public void processRequest_unknownGoodsBecauseDelisted() throws BurstException {
+    final HttpServletRequest req = QuickMocker.httpServletRequest();
+
+    final Goods mockGoods = mock(Goods.class);
+    when(mockGoods.isDelisted()).thenReturn(true);
+
+    final Account mockSenderAccount = mock(Account.class);
+
+    when(mockParameterService.getSenderAccount(eq(req))).thenReturn(mockSenderAccount);
+    when(mockParameterService.getGoods(eq(req))).thenReturn(mockGoods);
+
+    assertEquals(UNKNOWN_GOODS, t.processRequest(req));
+  }
+
+  @Test
+  public void processRequest_unknownGoodsBecauseWrongSellerId() throws BurstException {
+    final HttpServletRequest req = QuickMocker.httpServletRequest();
+
+    final Goods mockGoods = mock(Goods.class);
+    when(mockGoods.isDelisted()).thenReturn(false);
+    when(mockGoods.getSellerId()).thenReturn(1L);
+
+    final Account mockSenderAccount = mock(Account.class);
+    when(mockSenderAccount.getId()).thenReturn(2L);
+
+    when(mockParameterService.getSenderAccount(eq(req))).thenReturn(mockSenderAccount);
+    when(mockParameterService.getGoods(eq(req))).thenReturn(mockGoods);
+
+    assertEquals(UNKNOWN_GOODS, t.processRequest(req));
+  }
+
+  @Test
+  public void processRequest_missingDeltaQuantity() throws BurstException {
+    final HttpServletRequest req = QuickMocker.httpServletRequest(
+        new MockParam(DELTA_QUALITY_PARAMETER, null)
+    );
+
+    final Goods mockGoods = mock(Goods.class);
+    when(mockGoods.isDelisted()).thenReturn(false);
+    when(mockGoods.getSellerId()).thenReturn(1L);
+
+    final Account mockSenderAccount = mock(Account.class);
+    when(mockSenderAccount.getId()).thenReturn(1L);
+
+    when(mockParameterService.getSenderAccount(eq(req))).thenReturn(mockSenderAccount);
+    when(mockParameterService.getGoods(eq(req))).thenReturn(mockGoods);
+
+    assertEquals(MISSING_DELTA_QUANTITY, t.processRequest(req));
+  }
+
+  @Test
+  public void processRequest_deltaQuantityWrongFormat() throws BurstException {
+    final HttpServletRequest req = QuickMocker.httpServletRequest(
+        new MockParam(DELTA_QUALITY_PARAMETER, "Bob")
+    );
+
+    final Goods mockGoods = mock(Goods.class);
+    when(mockGoods.isDelisted()).thenReturn(false);
+    when(mockGoods.getSellerId()).thenReturn(1L);
+
+    final Account mockSenderAccount = mock(Account.class);
+    when(mockSenderAccount.getId()).thenReturn(1L);
+
+    when(mockParameterService.getSenderAccount(eq(req))).thenReturn(mockSenderAccount);
+    when(mockParameterService.getGoods(eq(req))).thenReturn(mockGoods);
+
+    assertEquals(INCORRECT_DELTA_QUANTITY, t.processRequest(req));
+  }
+
+  @Test
+  public void processRequest_deltaQuantityOverMaxIncorrectDeltaQuantity() throws BurstException {
+    final HttpServletRequest req = QuickMocker.httpServletRequest(
+        new MockParam(DELTA_QUALITY_PARAMETER, Integer.MIN_VALUE)
+    );
+
+    final Goods mockGoods = mock(Goods.class);
+    when(mockGoods.isDelisted()).thenReturn(false);
+    when(mockGoods.getSellerId()).thenReturn(1L);
+
+    final Account mockSenderAccount = mock(Account.class);
+    when(mockSenderAccount.getId()).thenReturn(1L);
+
+    when(mockParameterService.getSenderAccount(eq(req))).thenReturn(mockSenderAccount);
+    when(mockParameterService.getGoods(eq(req))).thenReturn(mockGoods);
+
+    assertEquals(INCORRECT_DELTA_QUANTITY, t.processRequest(req));
+  }
+
+  @Test
+  public void processRequest_deltaQuantityLowerThanNegativeMaxIncorrectDeltaQuantity() throws BurstException {
+    final HttpServletRequest req = QuickMocker.httpServletRequest(
+        new MockParam(DELTA_QUALITY_PARAMETER, Integer.MAX_VALUE)
+    );
+
+    final Goods mockGoods = mock(Goods.class);
+    when(mockGoods.isDelisted()).thenReturn(false);
+    when(mockGoods.getSellerId()).thenReturn(1L);
+
+    final Account mockSenderAccount = mock(Account.class);
+    when(mockSenderAccount.getId()).thenReturn(1L);
+
+    when(mockParameterService.getSenderAccount(eq(req))).thenReturn(mockSenderAccount);
+    when(mockParameterService.getGoods(eq(req))).thenReturn(mockGoods);
+
+    assertEquals(INCORRECT_DELTA_QUANTITY, t.processRequest(req));
+  }
+
 }
