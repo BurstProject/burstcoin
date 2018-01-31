c1b22d680a888ab6c166589ff3b4ec4a3377b302
Brabantian <brabantianburst@gmail.com>
2018-01-31 23:42:38 +0100

    TransactionProcessorImpl DI instead of calling Burst

--- src/brs/services/EscrowService.java
+++ src/brs/services/EscrowService.java
@@ -2,10 +2,13 @@ package brs.services;
 
 import brs.Escrow;
 import brs.db.BurstIterator;
+import java.util.Collection;
 
 public interface EscrowService {
 
   BurstIterator<Escrow> getAllEscrowTransactions();
 
   Escrow getEscrowTransaction(Long id);
+
+  Collection<Escrow> getEscrowTransactionsByParticipent(Long accountId);
 }
--- src/brs/services/impl/EscrowServiceImpl.java
+++ src/brs/services/impl/EscrowServiceImpl.java
@@ -1,18 +1,22 @@
 package brs.services.impl;
 
+import brs.Burst;
 import brs.Escrow;
 import brs.db.BurstIterator;
 import brs.db.BurstKey.LongKeyFactory;
 import brs.db.VersionedEntityTable;
 import brs.db.store.EscrowStore;
 import brs.services.EscrowService;
+import java.util.Collection;
 
 public class EscrowServiceImpl implements EscrowService {
 
   private final VersionedEntityTable<Escrow> escrowTable;
   private final LongKeyFactory<Escrow> escrowDbKeyFactory;
+  private final EscrowStore escrowStore;
 
   public EscrowServiceImpl(EscrowStore escrowStore) {
+    this.escrowStore = escrowStore;
     this.escrowTable = escrowStore.getEscrowTable();
     this.escrowDbKeyFactory = escrowStore.getEscrowDbKeyFactory();
   }
@@ -25,6 +29,11 @@ public class EscrowServiceImpl implements EscrowService {
   @Override
   public Escrow getEscrowTransaction(Long id) {
     return escrowTable.get(escrowDbKeyFactory.newKey(id));
+  }
+
+  @Override
+  public Collection<Escrow> getEscrowTransactionsByParticipent(Long accountId) {
+    return escrowStore.getEscrowTransactionsByParticipent(accountId);
   }
 
 }
