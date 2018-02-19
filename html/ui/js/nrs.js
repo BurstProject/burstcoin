dab80b79daabc544cf9e00f7367b9010e57c93ef
rico666 <bots@cryptoguru.org>
2018-02-19 14:24:36 +0100

    update bootstrap to 3.3.7

--- html/ui/js/nrs.js
+++ html/ui/js/nrs.js
@@ -1,6 +1,6 @@
 /**
- * @depends {3rdparty/jquery-2.1.0.js}
- * @depends {3rdparty/bootstrap.js}
+ * @depends {3rdparty/jquery.min.js}
+ * @depends {3rdparty/bootstrap.min.js}
  * @depends {3rdparty/big.js}
  * @depends {3rdparty/jsbn.js}
  * @depends {3rdparty/jsbn2.js}
