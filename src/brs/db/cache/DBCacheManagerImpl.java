02663c543ca83a01ac2a1030e5487a1990634709
ac0v <ac0v@cryptoguru.org>
2018-02-28 16:43:20 +0100

    allow to add multiple *generic* caches

--- src/brs/db/cache/DBCacheManagerImpl.java
+++ src/brs/db/cache/DBCacheManagerImpl.java
@@ -3,8 +3,11 @@ package brs.db.cache;
 import brs.Account;
 import brs.db.sql.DbKey;
 import brs.statistics.StatisticsManagerImpl;
+import java.util.HashMap;
+import java.util.Map;
 import org.ehcache.Cache;
 import org.ehcache.CacheManager;
+import org.ehcache.config.CacheConfiguration;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
@@ -17,13 +20,21 @@ public class DBCacheManagerImpl {
 
   private final boolean statisticsEnabled;
 
+  private final static HashMap<String, CacheConfiguration> caches = new HashMap<String, CacheConfiguration>(){
+    {
+      put("account", CacheConfigurationBuilder.newCacheConfigurationBuilder(DbKey.class, Account.class, ResourcePoolsBuilder.heap(100)).build());
+    }
+  };
+
   public DBCacheManagerImpl(StatisticsManagerImpl statisticsManager) {
     this.statisticsManager = statisticsManager;
     statisticsEnabled = true;
 
-    cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
-        .withCache("account", CacheConfigurationBuilder.newCacheConfigurationBuilder(DbKey.class, Account.class, ResourcePoolsBuilder.heap(100)).build()
-        ).build(true);
+    CacheManagerBuilder cacheBuilder = CacheManagerBuilder.newCacheManagerBuilder();
+    for (Map.Entry<String, CacheConfiguration> cache : caches.entrySet()) {
+      cacheBuilder = cacheBuilder.withCache(cache.getKey(), cache.getValue());
+    }
+    cacheManager = cacheBuilder.build(true);
   }
 
   public void close() {
@@ -31,12 +42,8 @@ public class DBCacheManagerImpl {
   }
 
   private Cache getEHCache(String name) {
-    switch (name) {
-      case "account":
-        return cacheManager.getCache(name, DbKey.class, Account.class);
-      default:
-        return null;
-    }
+    CacheConfiguration cacheConfiguration = caches.get(name);
+    return cacheManager.getCache(name, cacheConfiguration.getKeyType().getClass(), cacheConfiguration.getValueType().getClass());
   }
 
   public Cache getCache(String name) {
@@ -45,7 +52,11 @@ public class DBCacheManagerImpl {
   }
 
   public void flushCache() {
-    getEHCache("account").clear();
+    for (String cacheName : caches.keySet()) {
+      Cache cache = getEHCache(cacheName);
+      if ( cache != null )
+        cache.clear();
+    }
   }
 
 }
