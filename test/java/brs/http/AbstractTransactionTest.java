ce02b2fad73f318c734934e43443b497dad4910b
Brabantian <35079014+Brabantian@users.noreply.github.com>
2018-03-09 22:55:27 +0100

    Mockito and Powermock version updates (#163)
    * Logger name correction
    * Base for integration tests
    * API server for integration test gets enabled
    * Integration Test name change for Maven. Cut down in the Powermock deps
    * DevNullListenerTest
    * Account removed unused methods, marked methods as "can be moved"
    * AliasServiceImplTest added missing tests
    * Removed useless bit
    * AssetServiceImplTest; Some small sonar cleanups
    * Account event listeners should "plug in" on accountService, not Account
    * Account -> AccountServiceImpl
    * More Account -> AccountServiceImpl
    * More Account -> AccountServiceImpl
    * Completely moved Account event listeners
    * Further methods moving Account -> AccountServiceImpl
    * Sonar removed 2 vulnabs (and 1 hidden vulnab)
    * Removed 2 vulnabs in Genesis.java
    * Vulnabs kill in Curve25519.java
    * final static -> static final
    * final public -> public final
    * Duplicate Strings sonar
    * EscrowSignTest, CancelAskOrderTest
    * GetAskOrderIdsTest, GetAskOrdersTest
    * GetAccountSubscriptionsTest
    * GetAccountEscrowTransactionsTest
    * GetAccountsWithRewardRecipientTest
    * GetAllOpenBidOrdersTest; GetAssetTest
    * Sonar: Useless imports should be removed
    * Mockito and Powermock version updates

--- test/java/brs/http/AbstractTransactionTest.java
+++ test/java/brs/http/AbstractTransactionTest.java
@@ -1,24 +1,15 @@
 package brs.http;
 
+import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.anyShort;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import brs.Account;
 import brs.Attachment;
-import brs.Burst;
 import brs.BurstException;
-import brs.Transaction;
-import brs.Transaction.Builder;
-import brs.TransactionProcessor;
-import brs.TransactionType.DigitalGoods;
 import brs.common.AbstractUnitTest;
-import brs.common.TestConstants;
-import brs.services.ParameterService;
 import javax.servlet.http.HttpServletRequest;
 import org.json.simple.JSONStreamAware;
 import org.mockito.ArgumentCaptor;
@@ -33,7 +24,7 @@ public abstract class AbstractTransactionTest extends AbstractUnitTest {
   protected Attachment attachmentCreatedTransaction(TransactionCreationFunction r, APITransactionManager apiTransactionManagerMock) throws BurstException {
     final ArgumentCaptor<Attachment> ac = ArgumentCaptor.forClass(Attachment.class);
 
-    when(apiTransactionManagerMock.createTransaction(any(HttpServletRequest.class), any(Account.class), any(Long.class), anyLong(), ac.capture(), anyLong())).thenReturn(mock(JSONStreamAware.class));
+    when(apiTransactionManagerMock.createTransaction(any(HttpServletRequest.class), nullable(Account.class), nullable(Long.class), anyLong(), ac.capture(), anyLong())).thenReturn(mock(JSONStreamAware.class));
 
     r.apply();
 
