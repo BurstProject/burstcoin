5ad17d44bf4f912240f035cdc31e36a347f85a59
Brabantian <brabantianburst@gmail.com>
2018-05-01 12:40:48 +0200

    Some moments deserve it

--- src/brs/fluxcapacitor/FluxInt.java
+++ src/brs/fluxcapacitor/FluxInt.java
@@ -1,15 +1,22 @@
 package brs.fluxcapacitor;
 
+import static brs.Constants.MAX_PAYLOAD_LENGTH_PRE_DYMAXION;
+
+import brs.Constants;
 import brs.common.Props;
 import brs.fluxcapacitor.FluxHistory.Element;
 import java.util.Arrays;
 
 public enum FluxInt {
 
-  BLOCK_SIZE(new FluxValue<>(
-      new FluxHistory<>(255, Arrays.asList(new Element<>(500000, 1020))),
-      new FluxHistory<>(255, Arrays.asList(new Element<>(88000, 1020))),
-      Props.DEV_BLOCK_SIZE_SETTING));
+  MAX_NUMBER_TRANSACTIONS(new FluxValue<>(
+      new FluxHistory<>(255, Arrays.asList(new Element<>(HistoricalMoments.PRE_DYMAXION, 1020))),
+      new FluxHistory<>(255, Arrays.asList(new Element<>(HistoricalMoments.PRE_DYMAXION_TN, 1020))),
+      Props.DEV_BLOCK_SIZE_SETTING))
+  , MAX_PAYLOAD_LENGTH(new FluxValue<>(
+      new FluxHistory<>(Constants.MAX_PAYLOAD_LENGTH, Arrays.asList(new Element<>(HistoricalMoments.PRE_DYMAXION, MAX_PAYLOAD_LENGTH_PRE_DYMAXION))),
+      new FluxHistory<>(Constants.MAX_PAYLOAD_LENGTH, Arrays.asList(new Element<>(HistoricalMoments.PRE_DYMAXION_TN, MAX_PAYLOAD_LENGTH_PRE_DYMAXION)))
+  ));
 
   private FluxValue<Integer> flux;
 
---
+++ src/brs/fluxcapacitor/HistoricalMoments.java
@@ -0,0 +1,13 @@
+package brs.fluxcapacitor;
+
+class HistoricalMoments {
+
+
+  static final int POC2 = 500000;
+  static final int PRE_DYMAXION = 500000;
+  static final int DYMAXION = Integer.MAX_VALUE;
+
+  static final int POC2_TN = 88000;
+  static final int PRE_DYMAXION_TN = 88000;
+  static final int DYMAXION_TN = Integer.MAX_VALUE;
+}
