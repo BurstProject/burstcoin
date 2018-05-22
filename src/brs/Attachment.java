c987cd9fed3d4c865130c8cd3126f888893e5325
rico666 <bots@cryptoguru.org>
2018-05-22 15:50:21 +0200

    Kill the Multi-Out killer

--- src/brs/Attachment.java
+++ src/brs/Attachment.java
@@ -270,7 +270,7 @@ public interface Attachment extends Appendix {
     public Long getAmountNQT() {
       long amountNQT = 0;
       for ( ArrayList<Long> recipient : recipients ) {
-        Convert.safeAdd(amountNQT, recipient.get(1));
+        amountNQT = Convert.safeAdd(amountNQT, recipient.get(1));
       }
       return amountNQT;
     }
