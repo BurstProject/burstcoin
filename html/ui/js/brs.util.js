35015f585ac43b2c0f17bee0174fe23a0a6e95f8
Almurik <kullthevaluzian@gmail.com>
2018-07-22 23:01:08 +0200

    Fix BRS.convertToNQT bug
    The previous fix to the rounding issue of BRS.convertToNQT didnt take into account that  BRS.convertToNQT could have string as an argument (assumption was number only). It is fixed now.

--- html/ui/js/brs.util.js
+++ html/ui/js/brs.util.js
@@ -371,7 +371,11 @@ var BRS = (function(BRS, $, undefined) {
         }
     };
     BRS.convertToNQT = function(currency) {
+
+        if(!isNaN(currency))
         currency = currency.toFixed(8);  ///  this fixes rounding issues (for the Total field on modals)
+        else
+        currency = String(currency);
         var parts = currency.split(".");
 
         var amount = parts[0];
